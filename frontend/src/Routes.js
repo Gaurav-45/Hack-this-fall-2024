b0f4c2188da33363c0a682b79e2b8b7db499984a
Gaurav Yashwant Parulekar <gauravparulekar02@gmail.com>
2024-11-09 16:29:59 +0530

    topnav changes

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -3,10 +3,12 @@ import { Route, Switch } from "react-router-dom";
 import Detect from "./pages/Detect";
 import NotFound from "./pages/NotFound";
 import Database from "./pages/Database";
+import Home from "./pages/Home";
 
 const routing = ({ childProps }) => (
   <Switch>
-    <Route path="/" exact component={Detect} props={childProps} />
+    <Route path="/" exact component={Home} props={childProps} />
+    <Route path="/detect" exact component={Detect} props={childProps} />
     <Route path="/database" exact component={Database} props={childProps} />
     <Route component={NotFound} />
   </Switch>
