2751db8ee8ea7c348517a842f34925596c968ca9
mansidw <dwivedimav@gmail.com>
2024-11-09 23:58:00 +0530

    completed the botton navigation addition and pwa app

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -1,15 +1,20 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
+import About from "./pages/About";
 import Detect from "./pages/Detect";
 import NotFound from "./pages/NotFound";
+import Contact from "./pages/Contact";
 import Database from "./pages/Database";
 import Home from "./pages/Home";
 import YPredictor from "./components/YieldPrediction/YPredictor";
+import PPredictor from "./components/PricePrediction/PPredictor";
 
 const routing = ({ childProps }) => (
   <Switch>
     <Route path="/" exact component={Home} props={childProps} />
     <Route path="/detect" exact component={Detect} props={childProps} />
+    <Route path="/about" exact component={About} props={childProps} />
+    <Route path="/contact" exact component={Contact} props={childProps} />
     <Route path="/database" exact component={Database} props={childProps} />
     <Route
       path="/yieldpredictor"
@@ -17,6 +22,13 @@ const routing = ({ childProps }) => (
       component={YPredictor}
       props={childProps}
     />
+    <Route
+      path="/pricepredictor"
+      exact
+      component={PPredictor}
+      props={childProps}
+    />
+
     <Route component={NotFound} />
   </Switch>
 );
