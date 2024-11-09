5ea26b6f91bd1cfbb17c252bb90fb2375fb2a5a5
Gaurav Yashwant Parulekar <gauravparulekar02@gmail.com>
2024-11-09 18:59:57 +0530

    Added yeild prediction

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -4,12 +4,19 @@ import Detect from "./pages/Detect";
 import NotFound from "./pages/NotFound";
 import Database from "./pages/Database";
 import Home from "./pages/Home";
+import YPredictor from "./components/YieldPrediction/YPredictor";
 
 const routing = ({ childProps }) => (
   <Switch>
     <Route path="/" exact component={Home} props={childProps} />
     <Route path="/detect" exact component={Detect} props={childProps} />
     <Route path="/database" exact component={Database} props={childProps} />
+    <Route
+      path="/yieldpredictor"
+      exact
+      component={YPredictor}
+      props={childProps}
+    />
     <Route component={NotFound} />
   </Switch>
 );
